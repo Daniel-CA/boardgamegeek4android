fdf4ec09e3e3f30192451963841155d73d63de7e
Chris Comeaux <chris.comeaux@gmail.com>
2018-03-05 09:29:06 -0600

    Introduce a new description tab

--- app/src/main/java/com/boardgamegeek/ui/GameActivity.java
+++ app/src/main/java/com/boardgamegeek/ui/GameActivity.java
@@ -381,6 +381,16 @@ public class GameActivity extends HeroTabActivity {
 		private void updateTabs() {
 			tabs.clear();
 			tabs.add(new Tab(
+				R.string.title_description,
+				R.drawable.fab_log_play,
+				new TabListener() {
+					@Override
+					public void onFabClicked() {
+						onPlayFabClicked();
+					}
+				}
+			));
+			tabs.add(new Tab(
 				R.string.title_info,
 				R.drawable.fab_log_play,
 				new TabListener() {
@@ -427,6 +437,8 @@ public class GameActivity extends HeroTabActivity {
 		public Fragment getItem(int position) {
 			if (position < tabs.size()) {
 				switch (tabs.get(position).getTitleResId()) {
+					case R.string.title_description:
+						return GameDescriptionFragment.newInstance(gameId);
 					case R.string.title_info:
 						return GameFragment.newInstance(gameId, gameName, iconColor, darkColor);
 					case R.string.title_collection:
