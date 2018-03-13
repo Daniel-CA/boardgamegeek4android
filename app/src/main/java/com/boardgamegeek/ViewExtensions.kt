e70ea23b423d17106344956d051cb9ec7faf219e
Chris Comeaux <chris.comeaux@gmail.com>
2018-03-12 21:41:52 -0500

    Add an empty state for the game description tab

---
+++ app/src/main/java/com/boardgamegeek/ViewExtensions.kt
@@ -0,0 +1,38 @@
+package com.boardgamegeek
+
+import android.view.View
+import android.view.View.VISIBLE
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+
+fun View.fadeIn(animate: Boolean = true) {
+    if (visibility != VISIBLE) {
+        if (animate) {
+            startAnimation(AnimationUtils.loadAnimation(context, android.R.anim.fade_in))
+        } else {
+            clearAnimation()
+        }
+        visibility = VISIBLE
+    }
+}
+
+fun View.fadeOut(visibility: Int = VISIBLE, animate: Boolean = true) {
+    if (this.visibility == VISIBLE) {
+        if (animate) {
+            val animation = AnimationUtils.loadAnimation(context, android.R.anim.fade_out)
+            animation.setAnimationListener(object : Animation.AnimationListener {
+                override fun onAnimationStart(animation: Animation) {}
+
+                override fun onAnimationEnd(animation: Animation) {
+                    this@fadeOut.visibility = visibility
+                }
+
+                override fun onAnimationRepeat(animation: Animation) {}
+            })
+            startAnimation(animation)
+        } else {
+            clearAnimation()
+            this.visibility = visibility
+        }
+    }
+}
